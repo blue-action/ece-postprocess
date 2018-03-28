72741cf271af9e745255ee515dc3c1885abcc213
Ronald van Haren <R.vanHaren@esciencecenter.nl>
2018-03-28 15:03:45 +0200

    move everything over to python3

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+import sys
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def conf_path(name):
+  if sys.prefix == '/usr':
+    conf_path = os.path.join('/etc', name)
+  else:
+    conf_path = os.path.join(sys.prefix, 'etc', name)
+  return conf_path
+setup(
+    name = "ece-postprocess",
+    version = "0.1",
+    author = "Yang Liu, Ronald van Haren",
+    author_email = "y.liu@esciencecenter.nl",
+    description = ("A package for postprocessing and archiving EC-EARTH data"),
+    license = "Apache 2.0",
+    url = "https://github.com/blue-action/ece-postprocess",
+    packages=['ece_postprocess'],
+#    package_data={'pyfrbcatdb': ['mapping.json']},
+#    data_files=[(os.path.join(conf_path('pyfrbcatdb')), ['pyfrbcatdb/dbase.config'])],
+    scripts=['ece_postprocess/scripts/ece-postprocess'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=['python-dateutil', 'numpy', 'netCDF4',
+                      'matplotlib', 'pyproj', 'pygrib', 'configargparse'],
+)
